994da575b232777e1ef3ea461f58e22e2d06c4f2
abebe9849 <masamasa20001002@gmail.com>
2020-08-21 18:47:48 +0900

    適当にいれた

---
+++ cotoba_aud_only_model.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*- 
+
+import sys
+import cv2
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+import torchvision.models as models
+import torch.nn as nn
+from torch.optim import Adam, SGD
+from torch.utils.data import DataLoader
+
+class Model(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        
+        self.fc_new = nn.Linear(257*5,512)
+        self.out = nn.Linear(512,257*5)
+    
+    def forward(self, aud):
+        aud = aud.view(-1,257*5)
+        aud = self.fc_new(aud)
+        out_audio = self.out(aud)
+        #out_audio = aud.view(-1,257,5)
+        return out_audio
+    
